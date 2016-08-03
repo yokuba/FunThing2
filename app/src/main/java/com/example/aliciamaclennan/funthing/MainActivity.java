7d92d9778a7cdf0d1ff027693315eb235014ade4
Alicia MacLennan <aflyingfemme@gmail.com>
2016-08-02 19:30:55 -0700

    Refactor deprecated getDrawable() method

--- app/src/main/java/com/example/aliciamaclennan/funthing/MainActivity.java
+++ app/src/main/java/com/example/aliciamaclennan/funthing/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.aliciamaclennan.funthing;
 
 import android.os.Bundle;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
@@ -17,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         drawView = (DrawingView) findViewById(R.id.drawing);
         LinearLayout paintLayout = (LinearLayout)findViewById(R.id.paint_colors);
         currPaint = (ImageButton)paintLayout.getChildAt(0);
-        currPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
+        ResourcesCompat.getDrawable(getResources(), R.drawable.paint_pressed, null);
 
     }
 
