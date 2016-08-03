cb63b4759c2eb83a27564ea5f37ca04486e55af0
Alicia MacLennan <aflyingfemme@gmail.com>
2016-08-03 13:02:31 -0700

    Add layout/listener to main activity for choosing brush size

--- app/src/main/java/com/example/aliciamaclennan/funthing/MainActivity.java
+++ app/src/main/java/com/example/aliciamaclennan/funthing/MainActivity.java
@@ -7,8 +7,9 @@ import android.support.v4.content.res.ResourcesCompat;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.view.View.OnClickListener;
 
-public class MainActivity extends Activity implements View.OnClickListener{
+public class MainActivity extends Activity implements View.OnClickListener {
     private DrawingView drawView;
     private ImageButton currPaint, drawBtn;
     private float smallBrush, mediumBrush, largeBrush;
@@ -21,17 +22,18 @@ public class MainActivity extends Activity implements View.OnClickListener{
         smallBrush = getResources().getInteger(R.integer.small_size);
         mediumBrush = getResources().getInteger(R.integer.medium_size);
         largeBrush = getResources().getInteger(R.integer.large_size);
-        LinearLayout paintLayout = (LinearLayout)findViewById(R.id.paint_colors);
-        currPaint = (ImageButton)paintLayout.getChildAt(0);
-        drawBtn = (ImageButton)findViewById(R.id.draw_btn);
+        LinearLayout paintLayout = (LinearLayout) findViewById(R.id.paint_colors);
+        currPaint = (ImageButton) paintLayout.getChildAt(0);
+        drawBtn = (ImageButton) findViewById(R.id.draw_btn);
         drawBtn.setOnClickListener(this);
         ResourcesCompat.getDrawable(getResources(), R.drawable.paint_pressed, null);
+        drawView.setBrushSize(mediumBrush);
 
     }
 
-    public void paintClicked(View view){
+    public void paintClicked(View view) {
         //use chosen color
-        if(view!=currPaint) {
+        if (view != currPaint) {
             //update color
             ImageButton imgView = (ImageButton) view;
             String color = view.getTag().toString();
@@ -45,11 +47,41 @@ public class MainActivity extends Activity implements View.OnClickListener{
     }
 
     @Override
-    public void onClick(View v) {
-        if(v.getId()==R.id.draw_btn){
+    public void onClick(View view) {
+        if (view.getId() == R.id.draw_btn) {
             //draw button clicked
             final Dialog brushDialog = new Dialog(this);
             brushDialog.setTitle("Brush size:");
+            brushDialog.setContentView(R.layout.brush_choice);
+            ImageButton smallBtn = (ImageButton) brushDialog.findViewById(R.id.small_brush);
+            smallBtn.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    drawView.setBrushSize(smallBrush);
+                    drawView.setLastBrushSize(smallBrush);
+                    brushDialog.dismiss();
+                }
+            });
+            ImageButton mediumBtn = (ImageButton) brushDialog.findViewById(R.id.medium_brush);
+            mediumBtn.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    drawView.setBrushSize(mediumBrush);
+                    drawView.setLastBrushSize(mediumBrush);
+                    brushDialog.dismiss();
+                }
+            });
+
+            ImageButton largeBtn = (ImageButton) brushDialog.findViewById(R.id.large_brush);
+            largeBtn.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    drawView.setBrushSize(largeBrush);
+                    drawView.setLastBrushSize(largeBrush);
+                    brushDialog.dismiss();
+                }
+            });
+            brushDialog.show();
         }
     }
 }
