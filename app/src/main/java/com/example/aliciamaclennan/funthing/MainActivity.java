bff5861aa5d3b5bd5789a25b56fddb02440bbbed
Alicia MacLennan <aflyingfemme@gmail.com>
2016-08-03 10:54:48 -0700

    Add onClickView for brush size choice

--- app/src/main/java/com/example/aliciamaclennan/funthing/MainActivity.java
+++ app/src/main/java/com/example/aliciamaclennan/funthing/MainActivity.java
@@ -1,23 +1,30 @@
 package com.example.aliciamaclennan.funthing;
 
+import android.app.Activity;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.content.res.ResourcesCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements View.OnClickListener{
     private DrawingView drawView;
-    private ImageButton currPaint;
+    private ImageButton currPaint, drawBtn;
+    private float smallBrush, mediumBrush, largeBrush;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         drawView = (DrawingView) findViewById(R.id.drawing);
+        smallBrush = getResources().getInteger(R.integer.small_size);
+        mediumBrush = getResources().getInteger(R.integer.medium_size);
+        largeBrush = getResources().getInteger(R.integer.large_size);
         LinearLayout paintLayout = (LinearLayout)findViewById(R.id.paint_colors);
         currPaint = (ImageButton)paintLayout.getChildAt(0);
+        drawBtn = (ImageButton)findViewById(R.id.draw_btn);
+        drawBtn.setOnClickListener(this);
         ResourcesCompat.getDrawable(getResources(), R.drawable.paint_pressed, null);
 
     }
@@ -34,6 +41,15 @@ public class MainActivity extends AppCompatActivity {
 
             currPaint.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.paint_pressed, null));
             currPaint = (ImageButton) view;
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.draw_btn){
+            //draw button clicked
+            final Dialog brushDialog = new Dialog(this);
+            brushDialog.setTitle("Brush size:");
         }
     }
 }
